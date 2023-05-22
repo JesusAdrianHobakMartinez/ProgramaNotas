da3fefa4fdbc52db7adc8aae7f300c5a21d65b2c
JesusAdrianHobakMartinez <jesus.18070037@itsmotul.edu.mx>
2023-05-22 10:36:36 -0600

    prueva 10

--- resources/views/asignaturas.blade.php
+++ resources/views/asignaturas.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Asignaturas</title>
+        <title>Estas son tus Asignaturas</title>
     </head>
 
     <body>
@@ -13,22 +13,22 @@
             
             <table class = "tabletext-base mx-auto">
             <thead >
-                <tr class=" bg-emerald-500">
+                <tr class=" bg-purple-700">
 
-                <th class="border-black border-4"> # </th>
-                <th class="border-black border-4">Asignatura</th>
-                <th class="border-black border-4">Clave</th>
+                <th class="border-black border-2"> ID </th>
+                <th class="border-black border-2">Asignatura</th>
+                <th class="border-black border-2">Clave</th>
                 </tr>
             </thead>
             <tbody>
 
                 @foreach ($asignaturas as $asignatura)
-                <tr class="border-black border-4">
+                <tr class="border-black border-2">
 
-                <td class=" border-black border-4 mx-2  bg-white py-2 px-2">{{ $loop->iteration }}</td>
+                <td class=" border-black border-2 mx-2  bg-white py-2 px-2">{{ $loop->iteration }}</td>
                 <td class=" border-black border-2 mx-2  bg-white py-2 px-2">{{ $asignatura->nombre }}</td>
                 <td class=" border-black border-2 mx-2  bg-white py-2 px-2">{{ $asignatura->clave }}</td>
-                <td class=" border-black border-4 mx-2  bg-blue-50 py-2 px-2">
+                <td class=" border-black border-2 mx-2  bg-blue-50 py-2 px-2">
                     <a href="{{ route('subjects.show', ['id' => $asignatura]) }}">Detalles</a>
                     
                 </td>
@@ -37,12 +37,13 @@
                 @endforeach
 
                 <!--Card mensaje inicio-->          
-                <div class="w-1/4 pb-4 mx-auto px-5 my-10 border-4 border-indigo-50  bg-white rounded-sm">
+                <div class="w-1/4 pb-4 mx-auto px-5 my-10 rounded-sm">
 
-                    <!-- Información -->
-                    <div class="w-full mt-4 bg-lime-100  rounded-md">
-                        <h1 class="text-2xl  text-center font-bold  text-black">Tu carrera es {{$asignatura->ing}}</h1>
-                    </div>  
+
+                    <!-- Información --> 
+                    <span class="box-decoration-clone bg-gradient-to-r from-indigo-800 to-blue-500 text-white px-6 ...">
+                         Tu carrera es {{$asignatura->ing}}
+                        </span> 
                 
                 </div>
             </tbody>
--- resources/views/cornellnote/edit.blade.php
+++ resources/views/cornellnote/edit.blade.php
@@ -32,7 +32,7 @@
       <label class="block text-gray-700 text-sm font-bold mb-2" for="resumen">
         Resumen:
       </label>
-      <textarea class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="resumen" name="resumen" type="text" placeholder="{{$nota->resumen}}" ></textarea>
+      <textarea class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="resumen" name="resumen" type="text" placeholder="" ></textarea>
       @error('resumen')
         <div class="alert alert-danger mt-1 mb-1 text-red-700">{{ $message }}</div>
       @enderror
